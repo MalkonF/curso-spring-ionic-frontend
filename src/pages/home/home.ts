e7ee8a52a5f213f10a419402c8a7f8b3f6f9ed8d
anonymous <sudo777@protonmail.com>
2020-02-12 19:55:41 -0300

    Desabilitar menu na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 //vai conf a app p q essa classe seja uma pagina e vou poder referenciar o nome dela entre aspas(string)
 @IonicPage()
 @Component({
@@ -9,9 +10,18 @@ import { NavController, IonicPage } from 'ionic-angular';
 export class HomePage {
   /*No angular se vc declarar um componente no construtor da classe ela é injetada automaticamente.
   NavController permite vc navegar de uma pagina p outra. */
-  constructor (public navCtrl: NavController) {
+  constructor (public navCtrl: NavController, public menu: MenuController) {
 
+  }/*Desabilita o menu qnd entra na pagina. ionViewWill..faz parte do lifecycle do ionic */
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
   }
+  /*Qnd sair da pagina habilita o menu */
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+  }
+
+
   /*No typescript a classe, atributo, metodo tem q ser acessado precedido do this */
   login() {
     this.navCtrl.setRoot('CategoriasPage');//abre a page Categorias.html. CategoriasPage é o controlador de Categorias.html
