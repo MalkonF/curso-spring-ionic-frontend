77b9667a2c9808f341076d538b5fddac7a6b04f4
anonymous <sudo777@protonmail.com>
2020-02-18 18:45:58 -0300

    Botao de logout no menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   templateUrl: 'app.html'
@@ -13,14 +14,20 @@ export class MyApp {
 
   pages: Array<{ title: string, component: string }>;
 
-  constructor (public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor (
+    public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen,
+    public auth: AuthService
+  ) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: 'HomePage' },
       { title: 'Profile', component: 'ProfilePage' },
-      { title: 'Categorias', component: 'CategoriasPage' }
+      { title: 'Categorias', component: 'CategoriasPage' },
+      { title: 'Logout', component: '' }//aki fica sem component pq ele vai ser implementado no met openPage
     ];
 
   }
@@ -33,11 +40,17 @@ export class MyApp {
       this.splashScreen.hide();
     });
   }
+  /*Qnd o openPage for chamado e o obj p for o objeto de logout, executa o logout */
+  openPage(page: { title: string, component: string }) {//e necessario definir esses atributo p acessar atributos da page
+    switch (page.title) {
+      case 'Logout':
+        this.auth.logout();//apaga token do localStorage
+        this.nav.setRoot('HomePage');
+        break;
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+      default:
+        this.nav.setRoot(page.component);
+    }
   }
 }
 /*Este arquivo é o controlador da page app.html */
